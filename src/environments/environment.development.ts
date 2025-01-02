44879dc4362d467f9bc7a1298a9fc17d765f6d36
Gtom09 <gebin092002@gmail.com>
2025-01-02 12:25:01 +0530

    changes in package.json

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: true,
+  production: false,
   apiUrl: 'http://localhost:8080',
   ratesUrl: 'http://localhost:8080/rates',
   discountUrl: 'http://localhost:8080/discount',
